70ce849875d75d285e159889c65c05ac6064b430
SyahirahMF <syahirahmfaizal@gmail.com>
2023-04-11 18:06:11 +0800

    'message'

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express')
 const app = express()
 const port = 3000
@@ -27,4 +28,106 @@ app.get('/bye', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+=======
+const express = require('express')
+const app = express()
+const port = 3000
+
+ app.use(express.json())
+  
+  app.post('/login', (req, res) => {
+    console.log(req.body)
+
+    let result = login(req.body.username, 
+      req.body.password)
+
+    res.send(result)
+  })
+
+
+app.get('/', (req, res) => {
+  res.send('Hello UTeM!')
+})
+
+app.get('/bye', (req, res) => {
+    res.send('BYE UTeM!')
+  })
+
+  app.post('/register', (req, res) => {
+    
+    let result = register(
+      req.body.username,
+      req.body.password,
+      req.body.name,
+      req.body.email
+      )
+
+    res.send(result)
+  })
+
+
+
+  function login(reqUsername, reqPassword)
+  {
+   let matchUser = dbUsers.find ( x => x.username == reqUsername)
+  
+  if(!matchUser) return "User not found!"
+  if(matchUser.password == reqPassword)
+  {
+      return matchUser
+  }
+  else
+  {
+      return "Invalid password"
+  }
+  }
+  
+  //for(let i = 0; i <dbUsers.length; i++)
+  //console.log(login("NSF","password"))
+  //console.log(login("NAF","password"))
+  
+ 
+function login1(reqUsername, reqpassword)
+{
+let matchUser = dbUsers.find(user => user.username == reqUsername)
+console.log(matchUser)
+}
+
+let dbUsers = [
+
+  {
+      username: "NSF",
+      password : "123456",
+      name: "N S",
+      email: "b022110002@student.utem.edu.my"
+  }
+  ,
+  {
+      username: "NAF",
+      password : "789321",
+      name: "NF",
+      email: "b022110003@student.utem.edu.my"
+  }
+  ,
+  {
+      username: "MAF",
+      password : "453627",
+      name: "AF",
+      email: "b022110004@student.utem.edu.my"
+  }
+]
+
+function register(reqUsername, reqPassword, reqName, reqEmail)
+{
+    dbUsers.push({
+        username : reqUsername,
+        password : reqPassword,
+        name : reqName, 
+        email : reqEmail
+    })
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+>>>>>>> 1eb9fb7 (login and register)
 })
